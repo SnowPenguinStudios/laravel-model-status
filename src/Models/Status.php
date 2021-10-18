f899d4d06c59d27a6dbd2d49eccbca4e01767758
Peter S <peterrstanley@gmail.com>
2021-10-17 21:22:25 -0400

    Implemented Status Model order. Updated package to support PHP v7.3+. Updated package documentation for current functionality.

--- src/Models/Status.php
+++ src/Models/Status.php
@@ -10,6 +10,8 @@ class Status extends Model
 {
     use HasFactory;
 
+    protected $fillable = ['model', 'name', 'description', 'is_default', 'is_active', 'order'];
+
     public function scopeActive(Builder $builder): Builder
     {
         return $builder->where('is_active', true);
@@ -18,5 +20,10 @@ class Status extends Model
     public function scopeDefault(Builder $builder): Builder
     {
         return $builder->where('is_default', true);
+    }
+
+    public function scopeOrder(Builder $builder, string $direction = 'asc'): Builder
+    {
+        return $builder->orderBy('order', $direction);
     }
 }
--- tests/app/Models/StatusTest.php
+++ tests/app/Models/StatusTest.php
@@ -3,6 +3,7 @@
 namespace SnowPenguinStudios\LaravelModelStatus\Tests;
 
 use SnowPenguinStudios\LaravelModelStatus\Models\Status;
+use SnowPenguinStudios\LaravelModelStatus\Tests\Models\DataModel;
 
 class StatusTest extends TestCase
 {
@@ -34,5 +35,77 @@ class StatusTest extends TestCase
 
         $this->assertCount(1, Status::active()->get());
         $this->assertJson(collect($activeStatus), Status::active()->get());
+    }
+
+    /** @test */
+    public function provides_statuses_by_order_default_asc(): void
+    {
+        $statuses = collect();
+        for ($i = 0; $i < 4; $i++) {
+            $statuses->push(Status::factory()->active()->create(['order' => $i]));
+        }
+
+        $statusIds = $statuses->pluck('id');
+        $currentStatusIds = Status::active()->order()->get()->pluck('id');
+        $this->assertEquals($statusIds, $currentStatusIds);
+    }
+
+    /** @test */
+    public function provides_statuses_by_order_asc(): void
+    {
+        $statuses = collect();
+        for ($i = 0; $i < 4; $i++) {
+            $statuses->push(Status::factory()->active()->create(['order' => $i]));
+        }
+
+        $statusIds = $statuses->pluck('id');
+        $currentStatusIds = Status::active()->order('asc')->get()->pluck('id');
+        $this->assertEquals($statusIds, $currentStatusIds);
+    }
+
+    /** @test */
+    public function provides_statuses_by_order_desc(): void
+    {
+        $statuses = collect();
+        for ($i = 0; $i < 4; $i++) {
+            $statuses->push(Status::factory()->active()->create(['order' => $i]));
+        }
+
+        $statusIds = $statuses->sortByDesc('id')->pluck('id');
+        $currentStatusIds = Status::active()->order('desc')->get()->pluck('id');
+        $this->assertEquals($statusIds, $currentStatusIds);
+    }
+
+    /** @test */
+    public function can_mass_assign_attributes_on_create(): void
+    {
+        $data = collect([
+            'model' => DataModel::class,
+            'name' => 'Name',
+            'description' => 'Descriptions',
+            'is_default' => false,
+            'is_active' => true,
+            'order' => 1
+        ]);
+
+        $status = Status::create($data->toArray());
+        $this->assertJson($data, $status);
+    }
+
+    /** @test */
+    public function can_mass_assign_attributes_on_update(): void
+    {
+        $status = Status::factory()->active()->create(['model' => DataModel::class]);
+        $data = collect([
+            'model' => DataModel::class,
+            'name' => 'Name',
+            'description' => 'Descriptions',
+            'is_default' => false,
+            'is_active' => true,
+            'order' => 1
+        ]);
+
+        $status->update($data->toArray());
+        $this->assertJson($data, $status);
     }
 }
